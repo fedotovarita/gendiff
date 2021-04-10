43f8c4cec7ce74f924a3cb9068ac02bb7882610c
fedotovarita <rita161@rambler.ru>
2021-04-10 21:02:14 +0300

    Add format flag and usage instructions

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,6 +5,11 @@ const program = new Command();
 
 program
   .description('Compares two configuration files and shows a difference.')
-  .version('1.0.0', '-V, --version');
+  .version('1.0.0', '-V, --version')
+  .option('-f, --format [type]', 'output format')
+  .arguments('<filepath1> <filepath2>')
 
+  .action(() => {
+    console.log('Usage: gendiff <filepath1> <filepath2>');
+  });
 program.parse(process.argv);
