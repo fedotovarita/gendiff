e6bd5fee103efc0ad9f2b7c9d20168386355b6f9
fedotovarita <rita161@rambler.ru>
2021-04-13 02:47:33 +0300

    Fixed linter mistakes3

--- src/genDiff.js
+++ src/genDiff.js
@@ -1,29 +1,29 @@
 import _ from 'lodash';
 
 const genDiffs = (path1, path2) => {
-    const firstData = JSON.parse(path1);
-    const secondData = JSON.parse(path2);
+  const firstData = JSON.parse(path1);
+  const secondData = JSON.parse(path2);
 
-    const keys = _.uniq([...Object.keys(firstData), ...Object.keys(secondData)]).sort();
+  const keys = _.uniq([...Object.keys(firstData), ...Object.keys(secondData)]).sort();
 
-    const result = keys.reduce((acc, key) => {
-        if (firstData[key] === secondData[key]) {
-            acc.push(`    ${key}: ${firstData[key]}`);
-            return acc;
-        }
-        if (!(secondData[key])) {
-            acc.push(`  - ${key}: ${firstData[key]}`);
-            return acc;
-        }
-        if (!(firstData[key])) {
-            acc.push(`  + ${key}: ${secondData[key]}`);
-            return acc;
-        }
+  const result = keys.reduce((acc, key) => {
+    if (firstData[key] === secondData[key]) {
+      acc.push(`    ${key}: ${firstData[key]}`);
+      return acc;
+    }
+    if (!(secondData[key])) {
+      acc.push(`  - ${key}: ${firstData[key]}`);
+      return acc;
+    }
+    if (!(firstData[key])) {
+      acc.push(`  + ${key}: ${secondData[key]}`);
+      return acc;
+    }
 
-        acc.push(`  - ${key}: ${firstData[key]}`);
-        acc.push(`  + ${key}: ${secondData[key]}`);
-        return acc;
-    }, []);
-    return result;
+    acc.push(`  - ${key}: ${firstData[key]}`);
+    acc.push(`  + ${key}: ${secondData[key]}`);
+    return acc;
+  }, []);
+  return result;
 };
 export default genDiffs;
--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ import genDiffs from './genDiff.js';
 
 const readFile = (filepath) => fs.readFileSync(path.resolve(process.cwd(), filepath));
 
-const generateDiffs = (path1, path2) => { 
-    const firstPath = readFile(path1);
-    const secondPath = readFile(path2);
-    const result = genDiffs(firstPath, secondPath);;
-    return `{\n${result.join('\n')}\n}`;
-}
+const generateDiffs = (path1, path2) => {
+  const firstPath = readFile(path1);
+  const secondPath = readFile(path2);
+  const result = genDiffs(firstPath, secondPath);
+  return `{\n${result.join('\n')}\n}`;
+};
 
 export default generateDiffs;
