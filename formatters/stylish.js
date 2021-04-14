b3ac0d7cd508940afafaf5e6aa1bd8eaad69f7ae
fedotovarita <rita161@rambler.ru>
2021-04-14 22:59:10 +0300

    Fixed mistakes in stylish.js

--- formatters/stylish.js
+++ formatters/stylish.js
@@ -3,19 +3,19 @@
 const flatRender = (array) => {
   const renderedArray = array.map((elem) => {
     if (elem.status === 'unchanged') {
-      return `     ${ elem.key }: ${ elem.value }`;
+      return `    ${ elem.key }: ${ elem.value }`;
     }
     if (elem.status === 'deleted') {
-      return `   - ${ elem.key }: ${ elem.value }`;
+      return `  - ${ elem.key }: ${ elem.value }`;
     }
     if (elem.status === 'added') {
-      return `   + ${ elem.key }: ${ elem.value }`;
+      return `  + ${ elem.key }: ${ elem.value }`;
     }
     if (elem.status === 'updated') {
-      return `   - ${ elem.key }: ${ elem.oldValue } \n   + ${ elem.key }: ${ elem.value }`;
+      return `  - ${ elem.key }: ${ elem.oldValue }\n  + ${ elem.key }: ${ elem.value }`;
     }
     if (elem.status === 'nested') {
-      return `     ${ elem.key }: \n ${flatRender(elem.children)}`;
+      return `    ${ elem.key }: \n ${flatRender(elem.children)}`;
     }
     return '';
   });
